bcf99ac9d9e80e4444c40af21c1355f6ab1b1fe4
Brennan Romance <brennan.romance@gmail.com>
2020-12-14 18:16:00 -0500

    change schedule file for ec2 instance testing

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,26 +19,26 @@
 
 # Learn more: http://github.com/javan/whenever
 
-every :monday, :at => '5:40pm', by_timezone: 'Eastern Time (US & Canada)' do
+every :monday, :at => '6:40pm', by_timezone: 'Eastern Time (US & Canada)' do
   rake "add_record:add_time_record"
 end
 
-every :monday, :at => '5:41pm', by_timezone: 'Eastern Time (US & Canada)' do
+every :monday, :at => '6:41pm', by_timezone: 'Eastern Time (US & Canada)' do
   rake "add_record:add_time_record"
 end
 
-every :monday, :at => '5:42pm', by_timezone: 'Eastern Time (US & Canada)' do
+every :monday, :at => '6:42pm', by_timezone: 'Eastern Time (US & Canada)' do
   rake "add_record:add_time_record"
 end
 
-every :monday, :at => '5:43pm', by_timezone: 'Eastern Time (US & Canada)' do
+every :monday, :at => '6:43pm', by_timezone: 'Eastern Time (US & Canada)' do
   rake "add_record:add_time_record"
 end
 
-every :monday, :at => '5:44pm', by_timezone: 'Eastern Time (US & Canada)' do
+every :monday, :at => '6:44pm', by_timezone: 'Eastern Time (US & Canada)' do
   rake "add_record:add_time_record"
 end
 
-every :monday, :at => '5:45pm', by_timezone: 'Eastern Time (US & Canada)' do
+every :monday, :at => '6:45pm', by_timezone: 'Eastern Time (US & Canada)' do
   rake "add_record:add_time_record"
 end
