a629cc213c50cf6cc3a16421cc0c49b670847937
Brennan Romance <brennan.romance@gmail.com>
2020-12-14 20:20:15 -0500

    delete junk in schedule.rb file

--- config/schedule.rb
+++ config/schedule.rb
@@ -42,10 +42,3 @@ end
 every :monday, :at => '8:35pm', by_timezone: 'Eastern Time (US & Canada)' do
   rake "add_record:add_time_record"
 end
-
-  TimeRecord Load (0.3ms)  SELECT  "time_records".* FROM "time_records" LIMIT $1  [["LIMIT", 11]]
-=> #<ActiveRecord::Relation [#<TimeRecord id: 2, message: 
-"new message created 2020-12-15 at 01:00:03", created_at: "2020-12-15 01:00:04", 
-updated_at: "2020-12-15 01:00:04">]>
-
-# Begin Whenever generated tasks for: /home/ubuntu/cron_rails_tester/config/schedule.rb at: 2020-12-15 00:58:22 +0000
