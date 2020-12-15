f6777bc3b2dd9e7b3f6e704e988ae534d972749c
Brennan Romance <brennan.romance@gmail.com>
2020-12-14 20:19:19 -0500

    change schedule.rb file for ec2 crontab testing

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,26 +19,33 @@
 
 # Learn more: http://github.com/javan/whenever
 
-every :monday, :at => '8:10pm', by_timezone: 'Eastern Time (US & Canada)' do
+every :monday, :at => '8:30pm', by_timezone: 'Eastern Time (US & Canada)' do
   rake "add_record:add_time_record"
 end
 
-every :monday, :at => '8:11pm', by_timezone: 'Eastern Time (US & Canada)' do
+every :monday, :at => '8:31pm', by_timezone: 'Eastern Time (US & Canada)' do
   rake "add_record:add_time_record"
 end
 
-every :monday, :at => '8:12pm', by_timezone: 'Eastern Time (US & Canada)' do
+every :monday, :at => '8:32pm', by_timezone: 'Eastern Time (US & Canada)' do
   rake "add_record:add_time_record"
 end
 
-every :monday, :at => '8:13pm', by_timezone: 'Eastern Time (US & Canada)' do
+every :monday, :at => '8:33pm', by_timezone: 'Eastern Time (US & Canada)' do
   rake "add_record:add_time_record"
 end
 
-every :monday, :at => '8:14pm', by_timezone: 'Eastern Time (US & Canada)' do
+every :monday, :at => '8:34pm', by_timezone: 'Eastern Time (US & Canada)' do
   rake "add_record:add_time_record"
 end
 
-every :monday, :at => '8:15pm', by_timezone: 'Eastern Time (US & Canada)' do
+every :monday, :at => '8:35pm', by_timezone: 'Eastern Time (US & Canada)' do
   rake "add_record:add_time_record"
 end
+
+  TimeRecord Load (0.3ms)  SELECT  "time_records".* FROM "time_records" LIMIT $1  [["LIMIT", 11]]
+=> #<ActiveRecord::Relation [#<TimeRecord id: 2, message: 
+"new message created 2020-12-15 at 01:00:03", created_at: "2020-12-15 01:00:04", 
+updated_at: "2020-12-15 01:00:04">]>
+
+# Begin Whenever generated tasks for: /home/ubuntu/cron_rails_tester/config/schedule.rb at: 2020-12-15 00:58:22 +0000
